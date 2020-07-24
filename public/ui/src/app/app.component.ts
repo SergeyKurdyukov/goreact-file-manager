dbe5206edbcde31bcb49fa2a41abecc41efab0a9
Sergey Kurdyukov <sergey.kurdyukov.v@gmail.com>
2020-07-24 18:55:22 +0300

    Added Upload button and logic to send a file to the server

--- public/ui/src/app/app.component.ts
+++ public/ui/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FilesService } from './files.service';
+import { IFile } from './files.types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ui';
+  public files: IFile[];
+  private selectedFile: File;
+  constructor(private filesService: FilesService) {}
+
+  async ngOnInit() {
+    this.files = await this.filesService.getList();
+  }
+
+  public async uploadFile(event) {
+    this.selectedFile = event.target.files[0];
+    const result = await this.filesService.upload(this.selectedFile);
+    console.log('AppComponent.uploadFile result:', result);
+  }
 }
---
+++ public/ui/src/app/files.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { IFile } from './files.types';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilesService {
+
+  constructor(private http: HttpClient) { }
+  public getList(): Promise<IFile[]> {
+    return this.http.get<IFile[]>(`${environment.serverUrl}/files`).toPromise();
+  }
+
+  public upload(file: File): Promise<string> {
+    console.log('FilesService.upload', file);
+    const formData = new FormData();
+    formData.set('fileKey', file, file.name);
+    return this.http.post<string>(`${environment.serverUrl}/files`, formData).toPromise();
+  }
+}
