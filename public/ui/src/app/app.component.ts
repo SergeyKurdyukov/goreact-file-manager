3ff1bad85cd8637c817ab661b7078499a2da73b3
Sergey Kurdyukov <sergey.kurdyukov.v@gmail.com>
2020-07-25 15:57:03 +0300

    Added preview tags, minor styles changes, minor logic improvement

--- public/ui/src/app/app.component.ts
+++ public/ui/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FilesService } from './files.service';
 import { IFile } from './files.types';
+import { environment } from 'src/environments/environment';
+
+
 
 @Component({
   selector: 'app-root',
@@ -12,21 +15,24 @@ export class AppComponent implements OnInit {
   title = 'ui';
   public files: IFile[];
   public selectedFile: IFile;
+  public serverUrl = environment.serverUrl;
   private fileToUpload: File;
+
   constructor(private filesService: FilesService) {}
 
   async ngOnInit() {
     this.files = await this.filesService.getList();
-    // this.files = this.files.map(file => {
-    //   file.isVideo = file.extension === 'mp4';
-    //   return file;
-    // });
+    this.files = this.files.map(file => {
+      file.isVideo = file.extension === 'mp4';
+      return file;
+    });
   }
 
   public async uploadFile(event) {
     this.fileToUpload = event.target.files[0];
     const result = await this.filesService.upload(this.fileToUpload);
     console.log('AppComponent.uploadFile result:', result);
+    this.files = await this.filesService.getList();
   }
 
   public onFileClick(file: IFile) {
